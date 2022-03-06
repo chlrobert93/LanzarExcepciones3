d82fc5c64080384d0301652771bdffb3475d5f5f
Eder <chlrobert93@gmail.com>
2022-03-06 15:23:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace LanzarExcepciones3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, resultado;  
+
+
+            try
+            {
+
+                Console.WriteLine("Introduce el primer Número");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Introduce el segundo Número");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                resultado = num1 / num2;
+                Console.WriteLine("El resultado de la operación es: {0}",resultado);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error:  {0}", e.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
